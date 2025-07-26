5f6022351e49ff0cd1be8f9d5c622124c4aeab0a
Gary Beane <gbeane66@gmail.com>
2025-07-26 17:52:49 +1000

    Updated pyproject.toml

---
+++ target/debug/deps/pytmat-567e4e84ed8f797c.d
@@ -0,0 +1,5 @@
+/home/gbea0001/rust/pytmat/target/debug/deps/libpytmat-567e4e84ed8f797c.rmeta: src/lib.rs
+
+/home/gbea0001/rust/pytmat/target/debug/deps/pytmat-567e4e84ed8f797c.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/pytmat-70aad47070d8822a.d
@@ -0,0 +1,5 @@
+/home/gbea0001/rust/pytmat/target/debug/deps/libpytmat-70aad47070d8822a.rmeta: src/lib.rs
+
+/home/gbea0001/rust/pytmat/target/debug/deps/pytmat-70aad47070d8822a.d: src/lib.rs
+
+src/lib.rs:
