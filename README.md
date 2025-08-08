3bb3e7a6420bb110ca6238677f09486e902dc405
Gary Beane <gbeane66@gmail.com>
2025-08-08 15:54:24 +1000

    Updated readme and pytest1.py

--- README.md
+++ README.md
@@ -44,18 +44,23 @@ import pytmat
 import matplotlib.pyplot as plt
 
 # Define layer thicknesses (nm)
-d = np.array([200])  # Example: 3 layers, with two seminfinite layers on either side of a 200 nm layer.
+d = np.array([200.0])  # Example: 3 layers, with two seminfinite layers on either side of a 200 nm layer.
 
 # Define complex refractive indices for each layer at each wavelength
 # Shape: (num_layers, num_wavelengths)
-n = np.array([
-    [1.0+0j, 1.0+0j, 1.0+0j],   # Layer 1, seminfinite air layer
-    [1.5+0j, 1.5+0j, 1.5+0j],   # Layer 2, 200 nm glass layer
-    [1.0+0j, 1.0+0j, 1.0+0j],   # Layer 3, seminfinite air layer
-])
+# Define number of wavelengths (300 for example)
+N = 300
+# Define refractive indices for 3 layers (air, glass, air) at 300 wavelengths
+# Here, we assume air (n=1.0) and a glass layer (n=1.5)
+# The first and last layers are seminfinite air layers, so their refractive index is 1.0
+# The middle layer is a glass layer with a refractive index of 1.5
+n_air = np.full(N, 1.0, dtype=np.complex128)
+n_glass = np.full(N, 1.5, dtype=np.complex128)
+
+n = np.array([n_air,n_glass,n_air])
 
 # Wavelengths (nm)
-wl = np.linspace(400, 700, 300)
+wl = np.linspace(400, 700, N)
 
 # Angle of incidence (radians) and polarization angle (radians)
 theta = 0.0  # normal incidence
@@ -77,6 +82,7 @@ ax.set_xlabel('Wavelength (nm)')
 ax.set_ylabel('Reflectance / Transmittance')
 plt.title('Multilayer Stack Reflection and Transmission')
 ax.legend()
+plt.show()
 ```
 
 ---
--- pytest1.py
+++ pytest1.py
@@ -1,39 +1,43 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import numpy as np
-from pytmat import *
+import pytmat
 import matplotlib.pyplot as plt
 
-wl = np.arange(0.3,900,1,dtype=np.float64)
-L = len(wl)
+# Define layer thicknesses (nm)
+d = np.array([200.0])  # Example: 3 layers, with two seminfinite layers on either side of a 200 nm layer.
 
-n1 = np.ones(L, dtype=np.complex128)
-n2 = 1.5*np.ones(L, dtype=np.complex128)
-n3 = 2.5*np.ones(L, dtype=np.complex128)
-n = np.array([n1,n3,n1],dtype=np.complex128)
+# Define complex refractive indices for each layer at each wavelength
+# Shape: (num_layers, num_wavelengths)
+# Define number of wavelengths (300 for example)
+N = 300
+# Define refractive indices for 3 layers (air, glass, air) at 300 wavelengths
+# Here, we assume air (n=1.0) and a glass layer (n=1.5)
+# The first and last layers are seminfinite air layers, so their refractive index is 1.0
+# The middle layer is a glass layer with a refractive index of 1.5
+n_air = np.full(N, 1.0, dtype=np.complex128)
+n_glass = np.full(N, 1.5, dtype=np.complex128)
+n = np.array([n_air,n_glass,n_air])
 
-d = np.array([500], dtype=np.float64)
+# Wavelengths (nm)
+wl = np.linspace(400, 700, N)
 
-phi = 0.0
-theta = 0.0
+# Angle of incidence (radians) and polarization angle (radians)
+theta = 0.0  # normal incidence
+phi = 0.0    # TE polarization
 
-new_data_te = DataPy(d,n,wl,theta,0.0)
-new_data_tm = DataPy(d,n,wl,theta,90.0)
+# Create the TMM data object
+data = pytmat.DataPy(d, n, wl, theta, phi)
 
-# print(new_data_te.get_phi())
+# Simulate the multilayer stack
+simulation = data.simulate()
 
-result_te = new_data_te.simulate()
-result_tm = new_data_tm.simulate()
+# Compute reflection and transmission spectra
+R, T = simulation.r, simulation.t
 
-# print(i.d,i.n,i.wl,i.theta,i.phi)
-colors = ['red','black']
-freq = 3e8/(wl*1e-6)*1e-12 # freq in THz
-fig,ax = plt.subplots()
-ax.plot(freq,result_te.t,label="T TE",color=colors[0])
-ax.plot(freq,result_te.r,label="R TE",color=colors[0],linestyle='--')
-ax.plot(freq,result_tm.t,label="T TM",color=colors[1])
-ax.plot(freq,result_tm.r,label="R TM",color=colors[1],linestyle='--')
-plt.legend()
-ax.set_xlim([0.1,3])
+fig, ax = plt.subplots()
+ax.plot(wl, R, label='Reflection',color='blue')
+ax.plot(wl, T, label='Transmission', color='orange')
+ax.set_xlabel('Wavelength (nm)')
+ax.set_ylabel('Reflectance / Transmittance')
+plt.title('Multilayer Stack Reflection and Transmission')
+ax.legend()
 plt.show()
-# output_array = input_array.copy()
